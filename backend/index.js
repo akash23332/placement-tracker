7866a65716a31cc206c2917dd7b797cf54350f8c
Akash Sharma <akak9781189@gmail.com>
2026-06-19 22:31:03 +0530

    Render port fix

--- backend/index.js
+++ backend/index.js
@@ -24,11 +24,14 @@ mongoose.connect("mongodb+srv://Akash:akashsharma@cluster0.rmwkrog.mongodb.net/?
 .catch((err)=>{
 console.log(err)
 })
+const PORT =
+process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(
+    `Server running on port ${PORT}`
+  );
 });
-
 
 
 
